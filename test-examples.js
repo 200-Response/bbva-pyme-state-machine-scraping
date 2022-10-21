373dca5fb0fae353cfa5c202663090f1aa3da8a5
manny <maguilar@200response.mx>
2022-10-21 18:36:41 -0500

    examples added

--- test-examples.js
+++ test-examples.js
@@ -1,12 +1,18 @@
+const AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./config.json');
+
 const dynamoService = require('./services/dynamo');
 
-const getDataFromDynamoDB = () => {
+const getDataFromDynamoDB = async () => {
 
     await dynamoService.getAll('pyme-dataset')
         .then(async result => {
             console.log(result);
         })
         .catch(error => {
-            console.log(error);
+            console.log("error on getAll: ",error);
         });
 };
+
+getDataFromDynamoDB();
